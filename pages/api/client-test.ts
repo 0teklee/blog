3f5b495b2a449b03716da1b082e073aff716f51b
0teklee <leetekwoo@gmail.com>
2022-08-04 01:16:09 +0900

    feat: prisma api 추가

--- pages/api/client-test.ts
+++ pages/api/client-test.ts
@@ -1,4 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../libs/client";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {};
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  title: string,
+  content: string
+) => {
+  const request = await client.post.create({
+    data: {
+      title: title,
+      content: content,
+    },
+  });
+  const response = res.json({
+    ok: true,
+  });
+};
+
+const sendPost = (title: string, content: string) => {};
+
+export default handler;
