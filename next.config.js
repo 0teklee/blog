c1fe4fd814fe70e156ad77a591c0fca5f1fce9bc
0teklee <leetekwoo@gmail.com>
2022-08-26 18:20:34 +0900

    chore: enviorment variable setting changed, deleted unused files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  env: {
+    CLOUD_NAME: "dolziw8fv",
+    GA_TRACKING_ID: "G-6R2HEL045P",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
