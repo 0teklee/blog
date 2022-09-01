e824850251d1969b00d66d48c90fff75d1f37739
0teklee <leetekwoo@gmail.com>
2022-09-01 17:12:19 +0900

    feat: Added Blog Post's Tag get api & Edit Post's data get api

---
+++ pages/api/getBlogEditPost.ts
@@ -0,0 +1,34 @@
+import prisma from "libs/prisma";
+import { IBlogItemNoDate } from "types/IBlogItem";
+
+const getBlogEditPost = async (
+  params: string | string[]
+): Promise<IBlogItemNoDate> => {
+  const query = Number(params);
+  try {
+    const postsDB = await prisma.post.findUnique({
+      where: {
+        id: query,
+      },
+      include: {
+        categories: {
+          select: {
+            name: true,
+          },
+        },
+        tags: {
+          select: {
+            tag: true,
+          },
+        },
+      },
+    });
+    const posts = JSON.parse(JSON.stringify(postsDB));
+
+    return posts;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default getBlogEditPost;
