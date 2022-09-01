6d27a4daf64cdf649ec1a3abf3232bfd594db1b0
0teklee <leetekwoo@gmail.com>
2022-09-01 17:19:14 +0900

    chore: fixed meta tag description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import MetaTag from "components/MetaTag";
 import HomeTemplate from "components/Template/Home/HomeTemplate";
-import React, { useState } from "react";
+import { IBlogMainItem } from "types/IBlogItem";
+
 import getMainPosts from "./api/getMainPosts";
 
-const Home = (props) => {
+const Home = (props: { posts: IBlogMainItem[] }) => {
   const { posts } = props;
+
   return (
     <>
       <MetaTag
         title="teklog"
         url="www.teklog.com"
-        description="TedLee's Tech Blog"
+        description="Tekwoo Lee's Tech Blog - 프론트엔드 개발자 이택우의 기술블로그 "
       />
       <HomeTemplate posts={posts} />
     </>
