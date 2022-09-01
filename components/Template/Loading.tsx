2b6d8203bd6ef8e4f1a9f02d0e8c211442370858
0teklee <leetekwoo@gmail.com>
2022-09-01 17:27:32 +0900

    feat: added fallback page

---
+++ components/Template/Loading.tsx
@@ -0,0 +1,32 @@
+import Layout from "components/Atom/Layout";
+import Title from "components/Atom/Title";
+import Image from "next/image";
+import styled from "styled-components";
+import { theme } from "styles/theme";
+
+const Loading = () => {
+  return (
+    <Layout padding="8rem 2rem 5rem 2rem" mobilePadding="5rem 1rem 3rem 1rem">
+      <__Flex>
+        <Title title="Loading..." />
+        <Image
+          src="https://res.cloudinary.com/dolziw8fv/image/upload/v1661963643/bfb2651194bf749bc45221a83bb3648c_exkwpz.png"
+          alt="hummingbird"
+          width="200px"
+          height="200px"
+        />
+        <h4>
+          If it takes longer than a minute... you are trying to access wrong
+          URL...
+        </h4>
+      </__Flex>
+    </Layout>
+  );
+};
+
+export default Loading;
+
+const __Flex = styled.div`
+  ${theme.displayFlex("center", "center", "column")}
+  font-family: "proxima-nova", sans-serif;
+`;
