af05b8ce4596c031b82cf05102e805f67ea82607
tedlee <ted.lee@bclabs.co.kr>
2023-01-23 03:51:41 +0900

    fixed: fixed meta og img

--- components/MetaTag.tsx
+++ components/MetaTag.tsx
@@ -14,7 +14,7 @@ const MetaTag = ({
   title,
   description = "Front End Dev Tek.Lee's Tech Blog",
   url,
-  img = "https://res.cloudinary.com/dolziw8fv/image/upload/v1661494737/preset-pic-7_lnwlib.jpg",
+  img = "https://res.cloudinary.com/dolziw8fv/image/upload/v1674413434/DALL_E_2023-01-23_03.46.28_-_a_sceintific_photo_of_the_most_nostalgic_sundog_during_sunset_on_ocean_provia_400_rillrw.png",
 }: Props) => {
   return (
     <>
