64e91ddd6fc850177b5f41e1a44629399b9bf5fb
tedlee <ted.lee@bclabs.co.kr>
2023-01-24 22:37:40 +0900

    fix: fixed api error

--- pages/api/postBlog.ts
+++ pages/api/postBlog.ts
@@ -1,26 +1,22 @@
 import prisma from "libs/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
-import { stringify } from "querystring";
 import { IBlogItemPost } from "types/IBlogItem";
 
 const postBlog = async (req: NextApiRequest, res: NextApiResponse) => {
   const data: IBlogItemPost = req.body;
-  const { tag: tagIn, category, ...rest } = data;
+  const { category, ...rest } = data;
 
-  const tagsReqData = tagIn.map((item: string) => {
-    return {
-      where: { tag: item },
-      create: { tag: item },
-    };
-  });
+  // const tagsReqData = tagIn.map((item: string) => {
+  //   return {
+  //     where: { tag: item },
+  //     create: { tag: item },
+  //   };
+  // });
 
   try {
     const result = await prisma.post.create({
       data: {
         ...rest,
-        // tags: {
-        //   connectOrCreate: tagsReqData,
-        // },
         categories: {
           connectOrCreate: {
             where: { name: category },
@@ -31,8 +27,8 @@ const postBlog = async (req: NextApiRequest, res: NextApiResponse) => {
     });
     res.status(200).json(result);
   } catch (err) {
-    // console.log(err);
-    res.status(403).json({ err: err.message });
+    console.log(err);
+    return res.status(403).json({ err: err.message });
   }
 };
 
