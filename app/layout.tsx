1ed3cee35b483ca643de6ea357d9d10dda31594a
0teklee <leetekwoo@gmail.com>
2024-03-24 00:20:33 +0900

    add: RootLayout 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,29 @@ import React, { ReactNode } from "react";
 import { GlobalStyles } from "../styles/global-styles";
 import NextAuthSessionProvider from "../components/common/providers/NextAuthSessionProvider";
 import RecoilProvider from "../components/common/providers/RecoilProvider";
+import { clsx } from "clsx";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <NextAuthSessionProvider>
-      <GlobalStyles />
-      <RecoilProvider>{children}</RecoilProvider>
-    </NextAuthSessionProvider>
+    <html>
+      <head></head>
+      <body>
+        <NextAuthSessionProvider>
+          <GlobalStyles />
+          <RecoilProvider>
+            <main
+              className={clsx(
+                "w-full px-4 py-10",
+                "tablet:px-12 tablet:py-10",
+                "lg:px-20 lg:py-12",
+              )}
+            >
+              {children}
+            </main>
+          </RecoilProvider>
+        </NextAuthSessionProvider>
+      </body>
+    </html>
   );
 };
 
