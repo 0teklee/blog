4c29649b177373049a049a499710e2119007ecf7
0teklee <leetekwoo@gmail.com>
2024-03-24 11:40:56 +0900

    add: common/values 상수 추가

---
+++ components/common/values.ts
@@ -0,0 +1,9 @@
+export const HEADER_LFET_ITEMS = [
+  { id: "info_link", item: "info", path: "/info" },
+  { id: "gallery_link", item: "gallery", path: "/gallery" },
+];
+
+export const HEADER_RIGHT_ITEMS = [
+  { id: "archive_link", item: "archive", path: "/archive" },
+  { id: "blog_link", item: "blog", path: "/blog?page=1" },
+];
