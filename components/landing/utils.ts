1b0848262b920abf85e20d1e0a8132bb68f67e21
0teklee <leetekwoo@gmail.com>
2024-03-30 02:00:02 +0900

    fix: landing image position

--- components/landing/utils.ts
+++ components/landing/utils.ts
@@ -1,13 +1,15 @@
 export const getYPositionClass = (index: number) => {
   switch (index) {
+    case 0:
+      return "lg:mt-6";
     case 1:
-      return "lg:mt-12";
-    case 2:
       return "lg:mt-20";
+    case 2:
+      return "lg:mb-12";
     case 3:
-      return "lg:mt-28";
-    case 4:
       return "lg:mt-16";
+    case 4:
+      return "lg:mt-32";
     case 5:
       return "lg:mt-32";
     default:
