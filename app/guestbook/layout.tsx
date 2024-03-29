cb49280d6e84f8c78d0b113bdd5cf26add8ba727
0teklee <leetekwoo@gmail.com>
2024-03-29 22:18:40 +0900

    refactor: guestbook page - applied layout

---
+++ app/guestbook/layout.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from "react";
+import ReactQueryProvider from "components/common/providers/ReactQueryProvider";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return <ReactQueryProvider>{children}</ReactQueryProvider>;
+};
+
+export default Layout;
