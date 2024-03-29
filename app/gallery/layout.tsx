eebeee2f40de642233a1979fcd0c3d3b4101e09f
0teklee <leetekwoo@gmail.com>
2024-03-30 01:12:53 +0900

    chore: removed unused var

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode } from "react";
 import GallerySidebar from "components/gallery/GallerySidebar";
 
 const Layout = ({ children }: { children: ReactNode | ReactNode[] }) => {
-  const sample = null;
   return (
     <>
       <GallerySidebar />
