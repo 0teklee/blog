0cd0aa4862084d032d630ce9f6d6dde7dd644be3
0teklee <leetekwoo@gmail.com>
2024-03-31 14:49:24 +0900

    refactor: blog sidebar api data fetch moved

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,16 +1,20 @@
 import React, { ReactNode, Suspense } from "react";
-import getBlogCategoryList from "../../pages/api/getBlogCategoryList";
 import BlogSideBar from "../../components/blog/BlogSideBar";
 
-const Layout = async ({ children }: { children: ReactNode | ReactNode[] }) => {
-  const categoryList = await getBlogCategoryList();
+const Layout = async ({
+  children,
+  searchParams,
+}: {
+  children: ReactNode | ReactNode[];
+  searchParams: { category: string };
+}) => {
   return (
     <main
       className={`flex flex-col lg:flex-row lg:gap-x-12 w-full`}
       lang={`kr`}
     >
       <Suspense fallback={<>Loading...</>}>
-        <BlogSideBar categories={categoryList} />
+        <BlogSideBar searchParams={searchParams} />
       </Suspense>
       {children}
     </main>
