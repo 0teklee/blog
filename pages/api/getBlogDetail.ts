d227be09d834a3c1cff325dfddd9728584222641
0teklee <leetekwoo@gmail.com>
2024-04-17 01:10:38 +0900

    fix: detail api fix (get pagination id)

--- pages/api/getBlogDetail.ts
+++ pages/api/getBlogDetail.ts
@@ -1,19 +1,15 @@
 import prisma from "libs/prisma";
 import { IBlogGetDetail } from "@/components/blog/types";
-import getBlogDetailId from "./getBlogDetailId";
 
 const getBlogDetail = async (
   params: string | string[],
 ): Promise<IBlogGetDetail> => {
   const query = Number(params);
-  const idList = await getBlogDetailId();
-  const firstId = idList[0]?.id || 0;
 
   try {
-    const postsDB = await prisma.post.findMany({
-      take: Number(query) !== firstId ? 3 : 2,
-      cursor: {
-        id: Number(query) !== firstId ? Number(query) - 1 : Number(query),
+    const mainPost = await prisma.post.findUnique({
+      where: {
+        id: query,
       },
       select: {
         id: true,
@@ -28,28 +24,52 @@ const getBlogDetail = async (
       },
     });
 
-    const postDetail: IBlogGetDetail["detail"] = JSON.parse(
-      JSON.stringify(
-        postsDB.find(
-          (item) =>
-            item.id === Number(query) && item.categories.name !== "daily",
-        ),
-      ),
-    );
+    const navPosts = await Promise.all([
+      prisma.post.findFirst({
+        where: {
+          AND: [
+            { id: { lt: query } },
+            { categories: { name: { not: "daily" } } },
+          ],
+        },
+        orderBy: { id: "desc" },
+        take: 1,
+        select: {
+          id: true,
+          title: true,
+          content: true,
+          createdAt: true,
+          categories: true,
+        },
+      }),
+      prisma.post.findFirst({
+        where: {
+          AND: [
+            { id: { gt: query } },
+            { categories: { name: { not: "daily" } } },
+          ],
+        },
+        orderBy: { id: "asc" }, // 오름차순 정렬하여 가장 가까운 다음 ID 선택
+        take: 1,
+        select: {
+          id: true,
+          title: true,
+          content: true,
+          createdAt: true,
+          categories: true,
+        },
+      }),
+    ]);
 
-    const postPrevNext: IBlogGetDetail["nav"] = JSON.parse(
-      JSON.stringify(
-        postsDB.filter(
-          (item) =>
-            item.id !== Number(query) && item.categories.name !== "daily",
-        ),
-      ),
-    );
+    const nav = navPosts.filter((post) => post !== null);
 
-    return { detail: postDetail, nav: postPrevNext };
+    return {
+      detail: mainPost || null,
+      nav,
+    };
   } catch (err) {
-    console.log(err);
-    return {} as IBlogGetDetail;
+    console.error("Error fetching blog detail", err);
+    return { detail: null, nav: [] };
   }
 };
 
