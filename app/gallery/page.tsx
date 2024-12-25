7044c518f8184b61a115a64f1dab17de6cfd632b
0teklee <leetekwoo@gmail.com>
2024-12-25 21:57:14 +0900

    refactor: split list component & applied suspense boundary

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,37 +1,21 @@
-import { cache } from "react";
-import getGalleryList from "pages/api/getGalleryList";
-import { GALLERY_2022_IMAGES, THEME_META_IMAGE } from "libs/constants";
-import GalleryItem from "components/gallery/GalleryItem";
-import { clsx } from "clsx";
+import { THEME_META_IMAGE } from "libs/constants";
 import Title from "components/common/Title";
-
-const getGalleryListCache = cache(getGalleryList);
+import GalleryListContents from "@/components/gallery/GalleryListContents";
+import { Suspense } from "react";
+import Loading from "@/components/common/Loading";
 
 const page = async ({
   searchParams: { category },
 }: {
   searchParams: { category: string };
 }) => {
-  const posts =
-    category === "~2022"
-      ? GALLERY_2022_IMAGES
-      : await getGalleryListCache(category || "Collection");
-
   return (
     <>
       <Title title="Gallery" customStyle={"text-center mb-5"} />
       <section className="md:pl-36 py-4 md:py-0">
-        <div className={clsx("gallery-list-col gap-y-[100px] pt-12")}>
-          {posts?.map((item, i) => (
-            <GalleryItem
-              key={`${item.title}_key_${i}`}
-              id={item.id}
-              title={item.title}
-              url={item.imgUrl}
-              createdAt={item.createdAt}
-            />
-          ))}
-        </div>
+        <Suspense fallback={<Loading style={`w-full`} />}>
+          <GalleryListContents category={category} />
+        </Suspense>
       </section>
     </>
   );
