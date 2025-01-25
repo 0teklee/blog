ca6c317cb975417d6ac5c1c7331580b37033f4be
0teklee <leetekwoo@gmail.com>
2025-01-26 01:26:11 +0900

    refactor: blog/layout lg:grid + portal for right aside

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,43 +1,22 @@
-import React, { ReactNode, Suspense } from "react";
-import BlogSideBar from "@/components/blog/BlogSideBar";
-import { LoaderCircle } from "lucide-react";
+import React, { ReactNode } from "react";
+import BlogSideBar from "@/components/blog/detail/BlogSideBar";
+import { cn } from "@/libs/utils";
 
 const Layout = async ({ children }: { children: ReactNode | ReactNode[] }) => {
   return (
     <main
-      className={`flex flex-col lg:flex-row pt-6 lg:gap-x-12 lg:pt-12 w-full lg:justify-center`}
+      className={cn(
+        "flex flex-col w-full pt-6 lg:pt-12",
+        "lg:grid grid-cols-16 lg:gap-x-8 lg:justify-center",
+      )}
     >
-      <Suspense
-        fallback={
-          <aside
-            className={`
-        flex flex-col items-stretch gap-5 
-        w-full mb-12 flex-shrink-0 
-        bg-background scrollbar-hide
-        lg:w-56 lg:pb-12 lg:sticky 
-        lg:top-20 lg:h-[calc(100vh-4rem)]
-        overflow-hidden
-        opacity-70
-        `}
-          >
-            <summary
-              className={`
-            flex justify-between
-            w-full pb-2 
-            text-lg font-normal font-sans 
-            cursor-pointer 
-            after:content-["⇣"]
-            `}
-            >
-              <span>categories</span>
-            </summary>
-            <LoaderCircle className={`pt-2 text-gray-300 animate-spin`} />
-          </aside>
-        }
+      <BlogSideBar className={`lg:col-span-3`} />
+      <section
+        className={`flex flex-col items-center w-full lg:col-span-10 lg:max-w-4xl`}
       >
-        <BlogSideBar />
-      </Suspense>
-      {children}
+        {children}
+      </section>
+      <aside id={"left-nav-portal"} className={`lg:col-span-3`} />
     </main>
   );
 };
