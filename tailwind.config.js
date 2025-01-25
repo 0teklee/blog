014df28b57de2c5a5a6d55514e96b6b718d2dc1a
0teklee <leetekwoo@gmail.com>
2025-01-26 01:33:59 +0900

    fix: added grid-cols-16

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        16: "repeat(16, minmax(0, 1fr))",
+      },
       screens: {
         tablet: "600px",
       },
