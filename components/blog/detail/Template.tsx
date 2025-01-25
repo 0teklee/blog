2abbdfa84fbc6516937a3796b1d54990050e983e
0teklee <leetekwoo@gmail.com>
2025-01-26 01:27:23 +0900

    refactor: reusable detail template

---
+++ components/blog/detail/Template.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from "react";
+import { cn } from "@/libs/utils";
+import dayjs from "dayjs";
+
+const DetailHeader = ({
+  id,
+  title,
+  categories,
+  createdAt,
+}: {
+  id: string;
+  title: string;
+  categories: { name: string };
+  createdAt: string;
+}) => (
+  <div className="w-full">
+    <h1 className="w-full mb-4 font-sans text-5xl break-all">{title}</h1>
+    <div className="flex justify-between w-full mt-4 font-semibold text-base">
+      <p>{dayjs(createdAt).format("YYYY/MM/DD")}</p>
+      <p>{`n°${id}`}</p>
+    </div>
+    <div className="flex justify-between w-full mt-12 mb-12 font-semibold text-sm text-gray-500 wrap">
+      <p>category : {categories.name}</p>
+    </div>
+  </div>
+);
+
+const DetailLayout = ({ children }: { children: ReactNode | ReactNode[] }) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col w-full gap-3",
+        "lg:items-center lg:py-5 lg:flex-grow lg:pt-0",
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { DetailHeader, DetailLayout };
