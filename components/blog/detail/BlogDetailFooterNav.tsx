1a1c600c72b6cc72718be4266cc0610777bb9da7
0teklee <leetekwoo@gmail.com>
2025-01-26 19:39:22 +0900

    fix: blog > post detail > footer nav fixed

--- components/blog/detail/BlogDetailFooterNav.tsx
+++ components/blog/detail/BlogDetailFooterNav.tsx
@@ -1,73 +1,81 @@
 import React, { cache } from "react";
-import { clsx } from "clsx";
 import dayjs from "dayjs";
 import Link from "next/link";
 import getBlogNav from "@/libs/api/getBlogNav";
+import { cn } from "@/libs/utils";
 
 const getCacheBlogNav = cache(getBlogNav);
 
 const BlogDetailFooterNav = async ({ id }: { id: string }) => {
   const [prev, next] = await getCacheBlogNav(id);
-
   return (
-    <>
-      <Link
-        className="unset-cursor mt-12 font-sans font-light hover:text-theme hover:bg-sign transition-all duration-500"
-        href={`/blog`}
-      >
-        ← go back to list
-      </Link>
-      <div className="w-full">
-        {next && (
-          <Link
-            key="next"
-            className={clsx(
-              "relative px-2 py-1 flex justify-between",
-              "w-full text-base cursor-pointer",
-              "hover:bg-gray-500 hover:text-white transition-all duration-500",
-            )}
-            href={`/blog/${next.id}`}
-          >
-            <p>next</p>
-            <p
-              className={`
+    <footer className={`w-full`}>
+      <nav className={"relative flex flex-col items-center "}>
+        <Link
+          className={cn(
+            "unset-cursor",
+            "mt-12 p-2",
+            "font-sans font-light",
+            "transition-all duration-500",
+            "hover:text-theme hover:animate-pulse",
+            `before:absolute before:left-1/3 before:content-["←"] before:mr-1`,
+          )}
+          href={`/blog`}
+        >
+          go back to list
+        </Link>
+        <div className="w-full">
+          {prev && (
+            <Link
+              key="prev"
+              className={cn(
+                "relative px-2 py-1 flex justify-between",
+                "w-full cursor-pointer",
+                "hover:bg-theme hover:text-background transition-all duration-500",
+              )}
+              href={`/blog/${prev.id}`}
+            >
+              <p>prev</p>
+              <p
+                className={`
                 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
                 max-w-50% lg:max-w-30%
                 text-sm line-clamp-1 overflow-ellipsis`}
+              >
+                {prev.title}
+              </p>
+              <p className="text-sm">
+                {dayjs(prev.createdAt).format("YYYY-MM-DD")}
+              </p>
+            </Link>
+          )}
+          {next && (
+            <Link
+              key="next"
+              className={cn(
+                "relative px-2 py-1 flex justify-between",
+                "w-full cursor-pointer",
+                "hover:bg-theme hover:text-background transition-all duration-500",
+              )}
+              href={`/blog/${next.id}`}
             >
-              {next.title}
-            </p>
-            <p className="text-sm">
-              {dayjs(next.createdAt).format("YYYY-MM-DD")}
-            </p>
-          </Link>
-        )}
-        {prev && (
-          <Link
-            key="prev"
-            className={clsx(
-              "relative px-2 py-1 flex justify-between",
-              "w-full font-sans text-base cursor-pointer",
-              "hover:bg-gray-500 hover:text-white transition-all duration-500",
-            )}
-            href={`/blog/${prev.id}`}
-          >
-            <p>prev</p>
-            <p
-              className={`
+              <p>next</p>
+              <p
+                className={`
                 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
                 max-w-50% lg:max-w-30%
                 text-sm line-clamp-1 overflow-ellipsis`}
-            >
-              {prev.title}
-            </p>
-            <p className="text-sm">
-              {dayjs(prev.createdAt).format("YYYY-MM-DD")}
-            </p>
-          </Link>
-        )}
-      </div>
-    </>
+              >
+                {next.title}
+              </p>
+              <p className="text-sm">
+                {dayjs(next.createdAt).format("YYYY-MM-DD")}
+              </p>
+            </Link>
+          )}
+        </div>
+      </nav>
+    </footer>
   );
 };
 
