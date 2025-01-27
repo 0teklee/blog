feb1e73386ebb21cd3d5d35f50b944d72b993631
0teklee <leetekwoo@gmail.com>
2025-01-28 02:38:21 +0900

    style: blog layout fixed

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -7,16 +7,24 @@ const Layout = async ({ children }: { children: ReactNode | ReactNode[] }) => {
     <main
       className={cn(
         "flex flex-col w-full pt-6 lg:pt-12",
-        "lg:grid grid-cols-16 lg:gap-x-8 lg:justify-center",
+        "lg:grid grid-cols-16 lg:gap-x-5 lg:justify-center",
       )}
     >
       <BlogSideBar className={`lg:col-span-3`} />
       <section
-        className={`flex flex-col items-center w-full lg:col-span-10 lg:max-w-4xl`}
+        className={cn(
+          "flex flex-col items-center",
+          "w-full",
+          "lg:col-span-10 lg:max-w-4xl",
+          "xl:max-w-none xl:col-start-5 xl:col-span-8",
+        )}
       >
         {children}
       </section>
-      <aside id={"left-nav-portal"} className={`lg:col-span-3`} />
+      <aside
+        id={"left-nav-portal"}
+        className={`lg:col-span-3 xl:col-start-[14]`}
+      />
     </main>
   );
 };
