1e46c6d9a105848d004dca0c1077f242f1304fa9
0teklee <leetekwoo@gmail.com>
2025-03-01 17:22:39 +0900

    fix: drizzle client

--- db/index.ts
+++ db/index.ts
@@ -4,5 +4,5 @@ import { drizzle } from "drizzle-orm/postgres-js";
 
 const connectionString = process.env.DATABASE_URL!;
 
-const client = postgres(connectionString, { ssl: true, prepare: false });
+const client = postgres(connectionString, { ssl: false, prepare: false });
 export const db = drizzle(client);
