81437d11152a13d45c84e662b698b5ae7fca364a
0teklee <leetekwoo@gmail.com>
2025-03-08 22:57:30 +0900

    refactor: blog/utils removed unused util, changed blog/* components to use blog/utils - no @/lib/utils

--- components/blog/utils.ts
+++ components/blog/utils.ts
@@ -1,30 +1,46 @@
-import { IBlogGetCategorySideBar } from "@/components/blog/types";
-import { DEFAULT_SKY, imgSrcReplaceReg } from "libs/constants";
+import {
+  DEFAULT_DATA_IMAGE,
+  DEFAULT_ERROR_IMAGE,
+  DEFAULT_ETC_IMAGE,
+  DEFAULT_HTTP_IMAGE,
+  DEFAULT_JS_IMAGE,
+  DEFAULT_NEXT_IMAGE,
+  DEFAULT_REACT_IMAGE,
+  DEFAULT_RECAP_IMAGE,
+  DEFAULT_TS_IMAGE,
+  THEME_META_IMAGE,
+} from "@/libs/constants";
 
-export const initialCategoryState = (categories: IBlogGetCategorySideBar[]) =>
-  categories
-    .map((item) => item.name)
-    .reduce(
-      (acc, item) => {
-        return { ...acc, [item]: false };
-      },
-      {} as { [key: string]: boolean },
-    );
+export const imgSrcReplaceReg = /<img[^>]+src=["']([^"']+)["'][^>]*>/i;
 
 export const getImgSrc = (content: string) => {
-  const matchSrc = content?.match(imgSrcReplaceReg);
-  const isImage =
-    content && matchSrc && matchSrc.some((item) => item.includes("cloudinary"));
+  const match = imgSrcReplaceReg.exec(content);
+  return match ? match[1].replace("http://", "https://") : null;
+};
 
-  const imgSrc =
-    isImage &&
-    matchSrc &&
-    matchSrc
-      .map((src) => src.slice(4, -1))[0]
-      .replace("http", "https")
-      .replaceAll(`"`, "");
-
-  return imgSrc || DEFAULT_SKY;
+export const getCategoryImgSrc = (category: string): string => {
+  switch (category) {
+    case "React":
+      return DEFAULT_REACT_IMAGE;
+    case "Next.js":
+      return DEFAULT_NEXT_IMAGE;
+    case "TypeScript":
+      return DEFAULT_TS_IMAGE;
+    case "JavaScript":
+      return DEFAULT_JS_IMAGE;
+    case ".etc":
+      return DEFAULT_ETC_IMAGE;
+    case "Recap":
+      return DEFAULT_RECAP_IMAGE;
+    case "Error":
+      return DEFAULT_ERROR_IMAGE;
+    case "HTTP":
+      return DEFAULT_HTTP_IMAGE;
+    case "Data Structures & Algorithms":
+      return DEFAULT_DATA_IMAGE;
+    default:
+      return THEME_META_IMAGE;
+  }
 };
 
 export const formatBlogContent = (content: string) =>
