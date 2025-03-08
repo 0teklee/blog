4b5662a1ab420ceee8bef79ea4f8b908648241d6
0teklee <leetekwoo@gmail.com>
2025-03-08 23:01:45 +0900

    refactor: moved getImgSrc to blog/utils. **TODO** less dependency on utils in this file - leave only "cn" if possible

--- libs/utils.ts
+++ libs/utils.ts
@@ -1,14 +1,4 @@
 import { IGuestbookPost } from "@/components/guestbook/types";
-import {
-  DEFAULT_ERROR_IMAGE,
-  DEFAULT_ETC_IMAGE,
-  DEFAULT_IMAGE,
-  DEFAULT_JS_IMAGE,
-  DEFAULT_NEXT_IMAGE,
-  DEFAULT_REACT_IMAGE,
-  DEFAULT_RECAP_IMAGE,
-  DEFAULT_TS_IMAGE,
-} from "@/libs/constants";
 import { unified } from "unified";
 import rehypeParse from "rehype-parse";
 import { visit } from "unist-util-visit";
@@ -81,36 +71,12 @@ export const htmlReplace = (content: string | null | undefined) => {
         .trim();
 };
 
-const getContentImg = (content: string): string => {
+const getContentImg = (content: string): string | undefined => {
   const contentReg = content.match(/<img [^>]*src="[^"]*"[^>]*>/gm);
-  const contentImg =
-    contentReg &&
-    contentReg
-      .map((x: string) => x.replace(/.*src="([^"]*)".*/, "$1"))[0]
-      .replace("http", "https")
-      .replace("httpss", "https");
-  return contentImg ?? DEFAULT_IMAGE;
-};
-
-const setCategoryPresetImg = (category: string): string => {
-  switch (category) {
-    case "React":
-      return DEFAULT_REACT_IMAGE;
-    case "Next.js":
-      return DEFAULT_NEXT_IMAGE;
-    case "TypeScript":
-      return DEFAULT_TS_IMAGE;
-    case "JavaScript":
-      return DEFAULT_JS_IMAGE;
-    case ".etc":
-      return DEFAULT_ETC_IMAGE;
-    case "Recap":
-      return DEFAULT_RECAP_IMAGE;
-    case "Error":
-      return DEFAULT_ERROR_IMAGE;
-    default:
-      return DEFAULT_IMAGE;
-  }
+  return contentReg
+    ?.map((x: string) => x.replace(/.*src="([^"]*)".*/, "$1"))[0]
+    .replace("http", "https")
+    .replace("httpss", "https");
 };
 
 function cn(...inputs: ClassValue[]) {
@@ -286,7 +252,6 @@ const generateFallbackText = (length: number): string => {
 };
 
 export {
-  setCategoryPresetImg,
   getContentImg,
   parseHTMLToString,
   cn,
