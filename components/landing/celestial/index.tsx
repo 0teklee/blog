b07fa84b0baa5692d0e4d3a1cf81d16e57e21490
0teklee <leetekwoo@gmail.com>
2025-03-08 16:30:17 +0900

    fix: fetch revalidate added

--- components/landing/celestial/index.tsx
+++ components/landing/celestial/index.tsx
@@ -3,7 +3,9 @@ import CelestialBlog from "@/components/landing/celestial/CelestialBlog";
 import CelestialLayout from "@/components/landing/celestial/CelestialLayout";
 
 const CelestialRSC = async () => {
-  const res = await fetch(`${process.env.BASE_URL}/api/main/celestial`);
+  const res = await fetch(`${process.env.BASE_URL}/api/main/celestial`, {
+    next: { revalidate: 60 * 60 * 1000 },
+  });
   const { posts, categories } = await res.json();
 
   return (
