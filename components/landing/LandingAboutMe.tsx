c2b19e06f5edfe444a1028257c6384bd28c4f51a
0teklee <leetekwoo@gmail.com>
2025-03-08 20:43:30 +0900

    chore: changed intro, link

--- components/landing/LandingAboutMe.tsx
+++ components/landing/LandingAboutMe.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import clsx from "clsx";
 import { useRouter } from "next/navigation";
+import { cn } from "@/libs/utils";
 
 const LandingAboutMe = () => {
   const [lang, setLang] = useState<"en" | "kr">("en");
@@ -51,73 +52,37 @@ const LandingAboutMe = () => {
         </div>
       </div>
       <div
-        className={clsx(
-          "h-50",
+        className={cn(
+          "relative",
+          "h-48",
           "max-w-xl",
-          "line-height[1.4] whitespace-pre-wrap",
-          "font-normal text-md",
-          "md:text-lg",
+          "whitespace-pre-wrap",
+          "font-normal text-md md:text-lg",
         )}
       >
-        <div
-          className={clsx(
-            "transition-opacity duration-1000",
-            isEn
-              ? "opacity-100 h-56 md:h-52"
-              : "opacity-0 h-0 -translate-x-[100vw]",
+        <p
+          className={cn(
+            "transition-all duration-500",
+            // "ease-in-out",
+            isEn ? "opacity-100" : "opacity-0 absolute",
             "md:text-lg",
           )}
         >
-          <p>
-            PASSIONATE ABOUT WEB TECHNOLOGIES & USER INTERFACE. CURRENTLY
-            WORKING AS A FRONT-END DEVELOPER. I AM ABLE TO SEE THE BEAUTY IN
-            CODING AND CONTINUE TO CHALLENGE MYSELF TO CREATE FINER CODES.
-          </p>
-          <br />
-          <div className={clsx(`after:content-["."]`)}>
-            <span>
-              AS A FORMER VISUAL ARTIST, I STILL TAKE PHOTOS FROM TIME TO TIME
-              AS WELL. YOU CAN CHECK OUT MY PHOTOS ON
-            </span>
-            <button
-              className={`ml-1 underline hover:text-blue-600 transition duration-100`}
-              onClick={() => router.push("/gallery")}
-            >
-              GALLERY
-            </button>
-          </div>
-          <br />
-        </div>
-        <div
-          className={clsx(
-            "transition-opacity duration-1000",
-            isKr
-              ? "opacity-100 h-56 md:h-52"
-              : "opacity-0 h-0 -translate-x-[100vw]",
+          PASSIONATE ABOUT WEB TECHNOLOGIES & USER INTERFACE. CURRENTLY WORKING
+          AS A FRONT-END DEVELOPER. I AM ABLE TO SEE THE BEAUTY IN CODING AND
+          CONTINUE TO CHALLENGE MYSELF TO CREATE FINER CODES.
+        </p>
+        <p
+          className={cn(
+            "transition-all duration-500",
+            isKr ? "opacity-100" : "opacity-0 absolute",
             "md:text-lg",
           )}
         >
-          <p>
-            유저 인터페이스에 관심이 많은 프론트 개발자 이택우입니다. 개발자가
-            되기 이전에는 조형예술을 전공하고, 시각예술가로 활동을 해왔습니다.
-          </p>
-          <br />
-          <div>
-            <span>
-              취미로는 사진 찍는 것과 여행하는 것을 좋아합니다. 제가 찍은
-              사진들은
-            </span>
-            <button
-              className={`mx-1 underline transition duration-100 hover:text-blue-600`}
-              onClick={() => router.push("/gallery")}
-            >
-              여기에서
-            </button>
-            <span>확인이 가능합니다.</span>
-          </div>
-        </div>
+          인터페이스에 관심이 많은 프론트 개발자 이택우입니다.
+        </p>
       </div>
-      <div className={clsx(`after:content-["."]`)}>
+      <div>
         <span>FOR MORE INFORMATION, PLEASE VISIT</span>
         <button
           className={`ml-1 underline duration-100 hover:text-blue-600`}
--- components/landing/celestial/CelestialLayout.tsx
+++ components/landing/celestial/CelestialLayout.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import Link from "next/link";
 
 const CelestialLayout = ({
   children,
@@ -6,9 +7,17 @@ const CelestialLayout = ({
   children: ReactNode | ReactNode[];
 }) => {
   return (
-    <section className={"w-full space-y-2"}>
+    <section className={"w-full space-y-5"}>
       <p className={"text-center font-bold"}>INDEX</p>
       {children}
+      <div className={`text-center mt-12`}>
+        <Link
+          className={"font-semibold underline hover:text-theme"}
+          href={`/blog`}
+        >
+          Posts
+        </Link>
+      </div>
     </section>
   );
 };
