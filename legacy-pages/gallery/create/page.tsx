7c297c3b56d9c79a28c49548223d633be446d568
0teklee <leetekwoo@gmail.com>
2025-09-27 20:04:45 +0900

    ********removed legacy********

--- legacy-pages/gallery/create/page.tsx
+++
@@ -1,6 +0,0 @@
-// import React from "react";
-// import GalleryCreateTemplate from "@/components/gallery/GalleryCreateTemplate";
-//
-// const page = () => <GalleryCreateTemplate />;
-//
-// export default page;
