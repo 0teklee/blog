64e6bfda04dad1c20a10b64645718d0955fdd2e7
0teklee <leetekwoo@gmail.com>
2026-03-01 23:26:03 +0900

    refactor: migrate 2 statics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,37 @@
 import "./globals.css";
 import React, { ReactNode } from "react";
-import NextAuthSessionProvider from "components/common/providers/NextAuthSessionProvider";
 import Header from "components/common/Header";
 import NightModeButton from "@/components/common/NightModeButton";
 import Footer from "components/common/Footer";
 import Script from "next/script";
 import { GA_TRACKING_ID } from "libs/gtag";
-import { Cormorant_Garamond, Inter, Noto_Sans_KR } from "next/font/google";
 
 import { Analytics } from "@vercel/analytics/react";
 import { clsx } from "clsx";
 import NightModeProvider from "@/components/common/providers/NightModeProvider";
 
-const cormorant = Cormorant_Garamond({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600"],
-});
-
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-});
-
-const notoSans = Noto_Sans_KR({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600"],
-});
+const cormorantClassName = "font-cormorant";
+const interClassName = "font-inter";
+const notoSansClassName = "font-noto";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <html className={inter.className} lang={`kr`} suppressHydrationWarning>
+    <html className={interClassName} lang={`kr`} suppressHydrationWarning>
       <head>
         <title>teklog</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="teklog - web dev & etc" />
         <link rel="canonical" href="https://www.teklog.site" key="canonical" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;500;600&family=Inter:wght@300;400;500;600;700&family=Noto+Sans+KR:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
         <Script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
@@ -51,24 +48,22 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         <Analytics />
       </head>
       <body>
-        <NextAuthSessionProvider>
-          <NightModeProvider>
-            <Header fonts={cormorant.className} />
-            <NightModeButton />
-            <div
-              className={clsx(
-                "w-full px-4 py-24",
-                "tablet:px-12",
-                "lg:px-20",
-                "bg-background text-primary selection:bg-theme selection:text-background",
-                notoSans.className,
-              )}
-            >
-              {children}
-            </div>
-            <Footer fonts={cormorant.className} />
-          </NightModeProvider>
-        </NextAuthSessionProvider>
+        <NightModeProvider>
+          <Header fonts={cormorantClassName} />
+          <NightModeButton />
+          <div
+            className={clsx(
+              "w-full px-4 py-24",
+              "tablet:px-12",
+              "lg:px-20",
+              "bg-background text-primary selection:bg-theme selection:text-background",
+              notoSansClassName,
+            )}
+          >
+            {children}
+          </div>
+          <Footer fonts={cormorantClassName} />
+        </NightModeProvider>
       </body>
     </html>
   );
